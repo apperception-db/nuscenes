26652be22126ba56e62b79461b702fde09c5b62c
Chanwut (Mick) Kittivorawong <30903997+chanwutk@users.noreply.github.com>
2022-10-15 01:38:34 -0700

    fix camera rotation (#3)
    * fix camera rotation
    
    * style: [CI] cleanup python notebooks
    
    Co-authored-by: Github Actions Bot <apperception-actions-bot@users.noreply.github.com>

--- utils/get_camera_rotation.py
+++ utils/get_camera_rotation.py
@@ -7,4 +7,4 @@ from pyquaternion import Quaternion
 def get_camera_rotation(
     camera_rotation: "List[float]", ego_rotation: "List[float]"
 ) -> "npt.NDArray":
-    return (Quaternion(camera_rotation) * Quaternion(ego_rotation)).q
+    return (Quaternion(ego_rotation) * Quaternion(camera_rotation)).q
