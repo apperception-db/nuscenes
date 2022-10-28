e73935132d2e2d796dbf319851c9b98b6e648114
Chanwut (Mick) Kittivorawong <chanwutk@gmail.com>
2022-10-28 14:08:32 -0700

    clean up duplicated keys

--- utils/filter_road.py
+++ utils/filter_road.py
@@ -5,15 +5,60 @@ from typing import Callable, Dict, Set
 
 def filter_road(path: str, tokens: "Set[str]") -> dict:
     val = validator(tokens)
+    t = transformer()
 
     out = {}
     for k, v in val.items():
         with open(os.path.join(path, k + ".json"), "r") as f:
             content = json.load(f)
         print("Filtering", k)
-        out[k] = [*filter(v, content)]
+        out[k] = [*map(t[k], filter(v, content))]
 
     return out
+
+
+def transformer() -> "Dict[str, Callable[[dict], dict]]":
+    def factory(**config) -> "Callable[[dict], dict]":
+        def v(e: dict) -> dict:
+            assert {*config.keys()} == {*e.keys()}
+            return {
+                k: (None if e[k] is None or v is None else (e[k] if len(v) == 0 else e[k][: -len(v)]))
+                for k, v in config.items()
+            }
+
+        return v
+
+    def roadSection(e: dict) -> dict:
+        assert {"id", "forwardLanes", "backwardLanes"} == {*e.keys()}
+        return {
+            'id': e['id'][: - len('_sec')],
+            'forwardLanes': [lane[: -len("_sec")] for lane in e['forwardLanes']],
+            'backwardLanes': [lane[: -len("_sec")] for lane in e['backwardLanes']]
+        }
+
+    return {
+        "intersection": factory(id="_inter", road=""),
+        "lane_LaneSec": factory(laneSec="_sec", lane=""),
+        "lane": factory(id=""),
+        "laneGroup_Lane": factory(laneGroup="", lane=""),
+        "laneGroup_opposite": factory(lane="", opposite=""),
+        "laneGroup": factory(id=""),
+        "laneSection": factory(
+            id="_sec",
+            laneToLeft="_sec",
+            laneToRight="_sec",
+            fasterLane="_sec",
+            slowerLane="_sec",
+            isForward=None,
+        ),
+        "polygon": factory(id="", polygon=None),
+        "road_laneGroup": factory(laneGroup="", road=""),
+        "road_roadSec": factory(roadSec="_sec", road=""),
+        "road": factory(id="", forwardLanes="", backwardLanes=""),
+        "roadSec_laneSec": factory(laneSec="_sec", roadSec="_sec"),
+        "roadSection": roadSection,
+        "segment": factory(polygonId="", heading=None, end=None, start=None),
+    }
 
 
 def validator(tokens: "Set[str]") -> "Dict[str, Callable[[dict], bool]]":
